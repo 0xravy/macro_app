d112f932904b62297b486cfa61d0e1800baf9e6f
0xravy <0xravy@gmail.com>
2024-01-25 00:52:34 +0300

    update front end & add keydown & fix some problems

--- src-tauri/src/functions/mouse_macro.rs
+++ src-tauri/src/functions/mouse_macro.rs
@@ -1,17 +1,44 @@
 use rdev::{simulate, Button, Event, EventType, Key};
 
 #[derive(Clone)]
-pub struct MousePos {
+pub struct MouseMap {
     x: f64,
     y: f64,
-    click: Option<Button>,
+    mouse_click: Option<Button>,
+    mouse_is_press: bool,
+    key: Key,
+    key_is_press: bool,
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Clone)]
-pub struct Lala {
+pub struct Send2Tuari {
     x: f64,
     y: f64,
-    click: String,
+    mouse_click: String,
+    mouse_is_press: bool,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
+pub struct Status {
+    comment: String,
+}
+
+impl Status {
+    fn recording() -> Self {
+        Self {
+            comment: "start new recording".to_string(),
+        }
+    }
+    fn stop_recording() -> Self {
+        Self {
+            comment: "stop & delete the recroding".to_string(),
+        }
+    }
+    fn run_recording() -> Self {
+        Self {
+            comment: "is run record now".to_string(),
+        }
+    }
 }
 
 fn send(event_type: &EventType) {
@@ -24,47 +51,88 @@ fn send(event_type: &EventType) {
 }
 
 pub fn mouse(turi_window: tauri::Window) {
-    let mut mouse_map: Vec<MousePos> = Vec::new();
+    let mut mouse_map: Vec<MouseMap> = Vec::new();
     let mut is_record: bool = false;
+    let mut is_press: bool = false;
     let mut last_x = 0_f64;
     let mut last_y = 0_f64;
     let mut last_click = Button::Unknown(0);
 
     let macro_loop = move |event: Event| {
         let w = turi_window.clone();
+        let string_click = format!("{:?}", last_click);
 
         match &event.event_type {
             EventType::MouseMove { x, y } => {
                 last_x = *x;
                 last_y = *y;
-                let string_click = format!("{:?}", last_click);
                 let _ = w.emit(
                     "mouse",
-                    Lala {
-                        x: *x,
-                        y: *y,
-                        click: string_click,
+                    Send2Tuari {
+                        x: last_x,
+                        y: last_y,
+                        mouse_click: string_click,
+                        mouse_is_press: is_press,
                     },
                 );
             }
             EventType::ButtonPress(btn) => {
+                is_press = true;
                 last_click = *btn;
                 if is_record {
-                    mouse_map.push(MousePos {
+                    mouse_map.push(MouseMap {
                         x: last_x,
                         y: last_y,
-                        click: Some(*btn),
+                        mouse_click: Some(*btn),
+                        mouse_is_press: is_press,
                     });
                 }
+                let _ = w.emit(
+                    "mouse",
+                    Send2Tuari {
+                        x: last_x,
+                        y: last_y,
+                        mouse_click: string_click,
+                        mouse_is_press: is_press,
+                    },
+                );
+            }
+            EventType::ButtonRelease(btn) => {
+                is_press = false;
+                last_click = *btn;
+                if is_record {
+                    mouse_map.push(MouseMap {
+                        x: last_x,
+                        y: last_y,
+                        mouse_click: Some(*btn),
+                        mouse_is_press: is_press,
+                    });
+                }
+                let _ = w.emit(
+                    "mouse",
+                    Send2Tuari {
+                        x: last_x,
+                        y: last_y,
+                        mouse_click: string_click,
+                        mouse_is_press: is_press,
+                    },
+                );
             }
             EventType::KeyPress(key) => {
                 if Key::KeyZ == *key {
-                    for m in &mouse_map {
-                        send(&EventType::MouseMove { x: m.x, y: m.y });
+                    w.emit("status", Status::run_recording()).ok();
+                    for mouse_obj in &mouse_map {
+                        send(&EventType::MouseMove {
+                            x: mouse_obj.x,
+                            y: mouse_obj.y,
+                        });
 
-                        if m.click.is_some() {
-                            send(&EventType::ButtonPress(m.click.unwrap()));
-                            send(&EventType::ButtonRelease(m.click.unwrap()));
+                        if mouse_obj.mouse_click.is_some() {
+                            if mouse_obj.mouse_is_press {
+                                send(&EventType::ButtonPress(mouse_obj.mouse_click.unwrap()));
+                            } else {
+                                send(&EventType::ButtonRelease(mouse_obj.mouse_click.unwrap()));
+                            }
                             std::thread::sleep(std::time::Duration::from_millis(300));
                         }
                     }
@@ -72,12 +140,22 @@ pub fn mouse(turi_window: tauri::Window) {
                 if Key::KeyC == *key {
                     is_record = false;
                     mouse_map.clear();
-                    println!("stop & delete the recroding {:?}", is_record);
+                    w.emit("status", Status::stop_recording()).ok();
                 }
                 if Key::KeyR == *key {
+                    mouse_map.clear();
                     is_record = true;
-                    println!("start recording {:?}", is_record);
+                    w.emit("status", Status::recording()).ok();
                 }
+                let _ = w.emit(
+                    "mouse",
+                    Send2Tuari {
+                        x: last_x,
+                        y: last_y,
+                        mouse_click: string_click,
+                        mouse_is_press: false,
+                    },
+                );
             }
             _ => (),
         };
--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
-const { invoke, event } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.tauri;
 
 const x = document.getElementById("x");
 const y = document.getElementById("y");
 const mouse_click = document.getElementById("mouse_click");
+const mouse_is_press = document.getElementById("mouse_is_press");
+const comment = document.getElementById("comment");
+
+invoke("rust_data");
+
+/**
+ * x: f64,
+ * y: f64,
+ * mouse_click: String,
+ * mouse_is_press: bool,
+ */
 
 
-const data = await invoke("rust_data");
-
-async function update(e) {
+window.__TAURI__.event.listen("mouse", e => {
     x.textContent = e.payload.x;
     y.textContent = e.payload.y;
-    mouse_click.textContent = e.payload.click;
-}
+    mouse_click.textContent = e.payload.mouse_click;
+    mouse_is_press.textContent = e.payload.mouse_is_press;
+});
 
-window.__TAURI__.event.listen("mouse", update);
-update();
+window.__TAURI__.event.listen("status", e => {
+    comment.textContent = e.payload.comment;
+});
+
